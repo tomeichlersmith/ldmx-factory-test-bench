e77a597102dc8348d01304229a53127f0a295d29
tomeichlersmith <eichl008@umn.edu>
2024-09-17 16:10:18 -0500

    only one library to laod

--- fave_things.cpp
+++ fave_things.cpp
@@ -7,12 +7,12 @@ int main(int argc, char* argv[]) {
     std::cout << argv[0] << " libpath libentry" << std::endl;
     return 0;
   }
-  std::cout << "manually loading libraries" << std::endl;
+  std::cout << "manually loading library" << std::endl;
   if (dlopen(argv[1], RTLD_NOW) == nullptr) {
     std::cerr << "Failed to load '" << argv[1] << "': " << dlerror() << std::endl;
     return 1;
   }
-  std::cout << "done loading libraries" << std::endl;
+  std::cout << "done loading library" << std::endl;
   std::string full_cpp_name{argv[2]};
   try {
     auto entry_ptr{LibraryEntry::Factory::get().make(full_cpp_name)};

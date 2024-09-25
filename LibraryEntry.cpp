e924c0ccd51dd5abe501edc5786f15fb7b9323e0
tomeichlersmith <eichl008@umn.edu>
2024-09-25 12:22:01 -0500

    delete LibraryEntry impl

--- LibraryEntry.cpp
+++
@@ -1,8 +0,0 @@
-#include "LibraryEntry.h"
-
-::LibraryEntry::Factory the_factory_
-    __attribute((init_priority(500)));
-
-::LibraryEntry::Factory& LibraryEntry::factory() {
-  return the_factory_;
-}

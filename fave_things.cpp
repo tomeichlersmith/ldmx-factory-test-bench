67f76ffb0ce19958ea0368ef87ac1cccd4f9e105
tomeichlersmith <eichl008@umn.edu>
2024-09-25 12:21:32 -0500

    have both separate and not-separate libs use the same CLI

--- fave_things.cpp
+++ fave_things.cpp
@@ -3,25 +3,21 @@
 #include <dlfcn.h>
 
 int main(int argc, char* argv[]) {
-#ifdef SEPARATE_LIB
   if (argc != 3) {
     std::cout << argv[0] << " libpath libentry" << std::endl;
     return 0;
   }
+#ifdef SEPARATE_LIB
   std::cout << "manually loading library" << std::endl;
   if (dlopen(argv[1], RTLD_NOW) == nullptr) {
     std::cerr << "Failed to load '" << argv[1] << "': " << dlerror() << std::endl;
     return 1;
   }
   std::cout << "done loading library" << std::endl;
-  std::string full_cpp_name{argv[2]};
 #else
-  if (argc != 2) {
-    std::cout << argv[0] << " libentry" << std::endl;
-    return 0;
-  }
-  std::string full_cpp_name{argv[1]};
+  std::cout << "library compiled into executable" << std::endl;
 #endif
+  std::string full_cpp_name{argv[2]};
   try {
     auto entry_ptr{LibraryEntry::Factory::get().make(full_cpp_name)};
     std::cout << entry_ptr->name() << std::endl;
